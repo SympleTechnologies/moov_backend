9cba0381152686ab86de14663b17c4f80987b78a
seunkoko <seunowonikoko@gmail.com>
2018-02-18 19:17:32 +0100

    Update seed_default_data command to accommodate moov, school and car owner

--- api/helper/default_data.py
+++ api/helper/default_data.py
@@ -3,7 +3,7 @@ import os
 from ..models import User, UserType
 
 def create_default_user_types():
-    user_types = ['admin', 'driver', 'student']
+    user_types = ['admin', 'driver', 'student', 'moov', 'school', 'car_owner']
     for user_type in user_types:
         user_type = UserType(
             title=user_type,
@@ -11,12 +11,11 @@ def create_default_user_types():
         )
         user_type.save()
 
-def create_default_user(user_type_id):
-    super_user = User(
-            user_type_id=user_type_id,
-            firstname="admin",
-            lastname="admin",
-            image_url="https://pixabay.com/en/blank-profile-picture-mystery-man-973461/",
-            email=os.environ.get('ADMIN_EMAIL')
-        )
-    super_user.save()
+def create_user(user_type_id, name, email):
+    return User(
+                user_type_id=user_type_id,
+                firstname=name,
+                lastname=name,
+                image_url="https://pixabay.com/en/blank-profile-picture-mystery-man-973461/",
+                email=email
+            )
