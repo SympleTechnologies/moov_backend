d79f9a79679c4db3d265d47f6d7f68d8d8a640fb
seunkoko <seunowonikoko@gmail.com>
2018-03-24 01:04:51 +0100

    Function to get distance between lats and longs

--- api/helper/common_helper.py
+++ api/helper/common_helper.py
@@ -1,6 +1,27 @@
+import math
+
+
 # common helper functions
 
-def is_empty(value):
-  if value == "" or value.strip() == "":
-    return True
-  return False
+def get_distance(lat1, lon1, lat2, lon2, unit):
+  """Get Distance
+  This method gets two distance between 2 latitude and longitude in 
+  both kilometers and nautical miles
+  """
+  radlat1 = math.pi * lat1/180
+  radlat2 = math.pi * lat2/180
+  theta = lon1 - lon2
+  radtheta = math.pi * theta/180
+  dist = (math.sin(radlat1) * math.sin(radlat2)) + \
+          (math.cos(radlat1) * math.cos(radlat2) * math.cos(radtheta))
+  dist = math.acos(dist)
+  dist = dist * 180/math.pi
+  dist = dist * 60 * 1.1515
+  
+  if unit.lower() == "k":
+    dist = dist * 1.609344
+    
+  if unit.lower() == "n":
+    dist = dist * 0.8684
+    
+  return int(round(dist, 0))
